80d804f75a57f4235a716fa568c3a69354adabb1
Steven Smith <smith84@llnl.gov>
2019-07-17 14:34:04 -0700

    Update clang and rtags setup

--- bin/pfsetenv.sh
+++ bin/pfsetenv.sh
@@ -163,8 +163,9 @@ case $(hostname) in
       # 2015/11/06
 
       source $EBSIM_APPS_DIR/cmake/3.9.4/setup.sh
-      source /usr/casc/EBSim/apps/rh7/clang/5.0.0/setup.sh 
-      source /usr/casc/EBSim/apps/rh7/gcc/8.2.0/setup.sh 
+      source /usr/casc/EBSim/apps/rh7/clang/7.1.0/setup.sh 
+      source /usr/casc/EBSim/apps/rh7/gcc/8.2.0/setup.sh
+      source /usr/casc/EBSim/apps/rh7/rtags/2.33/setup.sh
       source /usr/casc/EBSim/apps/rh7/valgrind/3.13.0/setup.sh
 
       if ( true )
